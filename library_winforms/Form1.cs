865229c7c5b6e26885d4ff9ec00b99d005c1cff5
LAPTOP-55LAT378\ekon-28 <ekon-28@bk.ru>
2021-06-07 02:00:30 +0500

    winforms_project

---
+++ library_winforms/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace library_winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            reportHistoryClassBindingSource1.DataSource = GetReportDataClass.GetResult(dateStart.Value.ToString(), dateEnd.Value.ToString());
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            buttonImport.Enabled = false;
+            ExportToExcelClass.ExportToXLSX(dataGridView);
+            buttonImport.Enabled = true;
+        }
+    }
+}
