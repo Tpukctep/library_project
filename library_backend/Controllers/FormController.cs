093d85f26c46399276a53f25f73887db03dbc385
LAPTOP-55LAT378\ekon-28 <ekon-28@bk.ru>
2021-06-08 01:09:31 +0500

    library_backend

---
+++ library_backend/Controllers/FormController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using library_back.Models;
+
+namespace library_back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormController : ControllerBase
+    {
+        IFormRepository rep;
+        public FormController(IFormRepository r)
+        {
+            rep = r;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Rootobject>> Post(Rootobject rootobject)
+        {
+            //try
+            //{
+                rep.Create(rootobject);
+                return Ok(rootobject);
+            //}
+            //catch { return StatusCode(500); }
+        }
+    }
+}
